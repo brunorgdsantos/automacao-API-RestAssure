370c5a7a895d3a34546257df4f57d8920d6865e9
brunorgdsantos <brunorgdsantos@gmail.com>
2025-02-24 00:07:32 -0300

    Alterações após execução de testes

--- pushAfterTests.sh
+++ pushAfterTests.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
 
-# Executando os testes
 mvn clean test
 
-# Verificando se os testes passaram
 if [ $? -eq 0 ]; then
     echo "Testes passaram. Realizando o push para o GitHub..."
 
-    # Adicionando mudanças
     git add .
 
-    # Commitando as mudanças
     git commit -m "Alterações após execução de testes"
 
-    # Realizando o push para o repositório remoto
-    git push origin main
+    git push origin --all main
 else
     echo "Testes falharam. Não será realizado o push."
 fi
